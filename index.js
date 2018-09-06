ae491a4df170719b984ee25aa37af093d1394bc4
Will <willwright90@gmail.com>
2018-09-06 17:58:02 +1000

    Pivoting to be a map extension class

--- index.js
+++ index.js
@@ -1,81 +1,93 @@
 const _ = require('lodash');
+const objectHash = require('object-hash');
+const assert = require('assert');
 
-exports.ObjectSet = class ObjectSet {
+exports.ObjectSet = class ObjectSet extends Map {
   // Can be initialised as an empty set
   // mySet = new ObjectSet()
   // or with an array as a parameter
   // mySet = newObjectSet([{a: 2}, {a: 3}])
+  //
   constructor(arr) {
-    this.members = [];
-    if (arr) {
-      arr.map(item => this.add(item));
+    assert(arr == null || Array.isArray(arr));
+    if (Array.isArray(arr)) {
+      super();
+      this.addAll(arr);
+    } else {
+      super();
     }
   }
 
-  // Returns a copy of an item if it is found in the set.
-  has(item) {
-    return this.members.find(member => _.isEqual(member, item));
+  has(obj) {
+    assert(_.isObject(obj));
+    const hash = objectHash(obj);
+    return super.has(hash) && _.isEqual(obj, super.get(hash));
   }
 
-  // Returns the index of an item in the set, if it exists.
-  indexOf(item) {
-    for (let i = 0; i < this.members.length; i++) {
-      if (_.isEqual(item, this.members[i]))
-        return i;
-    }
-    return -1;
-  }
-
-  // Attempts to add an item to a set, returns true if successful and false if not
-  add(item) {
-    // Only add regular objects
-    if (!_.isPlainObject(item))
+  add(obj) {
+    assert(_.isObject(obj));
+    const hash = objectHash(obj);
+    if (!super.has(hash)) {
+      super.set(hash, _.cloneDeep(obj));
+      return true;
+    } else {
       return false;
-    for (let i = 0; i < this.members.length; i++) {
-      if (_.isEqual(item, this.members[i]))
-        return false;
     }
-    this.members.push(item);
-    return true;
   }
   
-  // Adds all the items in the array to the set
   addAll(arr) {
-    return arr.map(item => this.add(item));
+    return arr.map(obj => this.add(obj));
   }
 
   // Returns true if all of the items in arr are members in the set, false if not
   hasAll(arr) {
-    for (let item of arr) {
-      if (this.has(item) === undefined)
+    for (let obj of arr) {
+      if (!this.has(obj))
         return false;
     }
     return true;
   }
 
-  // Returns the cardinality of the set
-  size() {
-    return this.members.length;
-  }
-
-  // Clears the set
-  clear() {
-    this.members = [];
-  }
-
-  // Removes an item from the set, returns true if successful, and false if not
-  delete(item) {
-    let index = this.indexOf(item);
-    if (index > -1) {
-      this.members.splice(index, 1);
-      return true;
-    }
-    return false;
+  // Removes an obj from the set
+  delete(obj) {
+    const hash = objectHash(obj);
+    return super.delete(hash);
   }
 
   // Returns a list of all of the members of the set
   getMembers() {
-    return this.members;
+    let arr = [];
+    for (let [hash, obj] of this) {
+      arr.push(_.cloneDeep(obj));
+    }
+    return arr;
   }
-};
 
+  // forEach functionality
+  forEach(callback) {
+    this.getMembers().map(callback);
+  }
+
+  toString() {
+    return '[object Set]';
+  }
+
+  // returns a new object set which is the union of this set and the set passed in as argument
+  union(otherSet) {
+    let unionSet = new ObjectSet();
+    otherSet.forEach(obj => {
+      unionSet.add(obj);
+    });
+    this.forEach(obj => {
+      if (!unionSet.has(obj))
+        unionSet.add(obj);
+    });
+    return unionSet;
+  }
+
+  // Overwriting these so as to make them inaccessible 
+  entries() { return undefined; }
+  get() { return undefined; }
+  keys() { return undefined; }
+  set() { return undefined; }
+};
